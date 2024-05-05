ef092041e80680df30becdc7ffacc9a5307d8fc5
jimmyjr69 <162795733+jimmyjr69@users.noreply.github.com>
2024-05-05 12:22:38 +0300

    Add files via upload

---
+++ libs/algorithms/algorithm.h
@@ -0,0 +1,14 @@
+//
+// Created by yaros on 18.02.2024.
+//
+
+#ifndef UNTITLED1_ALGORITHM_H
+#define UNTITLED1_ALGORITHM_H
+
+#include <stdio.h>
+
+void swap(void * a, void * b, size_t len);
+
+int max(int a, int b);
+
+#endif //UNTITLED1_ALGORITHM_H
